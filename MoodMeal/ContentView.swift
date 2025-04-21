46536c2c954fa9027be8ae858b734916006a7f7e
naimroca <naimroca24@gmail.com>
2025-04-21 23:11:43 +0200

    Initial Commit

---
+++ MoodMeal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodMeal
+//
+//  Created by Naím Rodriguez Caballero on 21.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodMeal/MoodMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMealApp.swift
+//  MoodMeal
+//
+//  Created by Naím Rodriguez Caballero on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
